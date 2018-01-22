9f718625e222ca9d527672249c4cd4072db56305
Joanne Han <jhan13@risd.edu>
2018-01-22 15:00:29 -0500

    eloquent js exercise

---
+++ 07_jquery exercise/eloquent.js
@@ -0,0 +1,25 @@
+/* exercise 1
+for (var tag = '#'; tag.length < 8; tag = tag + '#') {
+	console.log(tag);
+}
+*/
+ /* exercise 2
+for (var n = 1; n <= 100; n++) {
+  if (n % 3 === 0 && n % 5 === 0) {
+  	console.log ('FizzBuzz')
+  } if (n % 3 === 0 ) {
+  	console.log ('Fizz')
+  } if (n % 5 === 0) {
+  	console.log ('Buzz')
+  } else {
+  	console.log(n);
+  }
+} */
+
+///* exercise 3
+var board = '';
+
+for (var k=1; k<9; k++) {
+	console.log(k);
+}
+	//*/
