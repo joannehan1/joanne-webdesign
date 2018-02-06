957f2819783a5e3038dd1d477a5028f2dc999808
Joanne Han <jhan13@risd.edu>
2018-02-06 12:45:26 -0500

    compilation
    p5 was not working

--- exercises/eleven/thing/sketch.js
+++ exercises/eleven/thing/sketch.js
@@ -1,38 +1,116 @@
-var message = "tickle",
-  font,
-  bounds, // holds x, y, w, h of the text's bounding box
-  fontsize = 60,
-  x, y; // x and y coordinates of the text
+// The midi notes of a scale
+var notes = [ 60, 62, 64, 65, 67, 69, 71];
 
-function preload() {
-  font = loadFont('assets/SourceSansPro-Regular.otf');
-}
+// For automatically playing the song
+var index = 0;
+var song = [
+  { note: 0, duration: 400, display: "G" },
+  { note: 0, duration: 400, display: "G" },
+  { note: 4, duration: 400, display: "D" },
+  { note: 4, duration: 400, display: "D" },
+  { note: 5, duration: 400, display: "E" },
+  { note: 5, duration: 400, display: "E" },
+  { note: 4, duration: 400, display: "D" },
+  { note: 4, duration: 400, display: "D" },
+  { note: 3, duration: 400, display: "C" },
+  { note: 3, duration: 400, display: "C" },
+  { note: 2, duration: 400, display: "B" },
+  { note: 2, duration: 400, display: "B" },
+  { note: 1, duration: 400, display: "A" },
+  { note: 1, duration: 400, display: "A" },
+  { note: 0, duration: 400, display: "G" }
+];
+var trigger = 0;
+var autoplay = false;
+var osc;
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(720, 400);
+  var div = createDiv("Click to play notes or ")
+  div.id("instructions");
+  var button = createButton("play song automatically.");
+  button.parent("instructions");
+  // Trigger automatically playing
+  button.mousePressed(function() {
+    if (!autoplay) {
+      index = 0;
+      autoplay = true;
+    }
+  });
 
-  // set up the font
-  textFont(font);
-  textSize(fontsize);
+  // A triangle oscillator
+  osc = new p5.TriOsc();
+  // Start silent
+  osc.start();
+  osc.amp(0);
+}
 
-  // get the width and height of the text so we can center it initially
-  bounds = font.textBounds(message, 0, 0, fontsize);
-  x = width / 2 - bounds.w / 2;
-  y = height / 2 - bounds.h / 2;
+// A function to play a note
+function playNote(note, duration) {
+  osc.freq(midiToFreq(note));
+  // Fade it in
+  osc.fade(0.5,0.2);
+
+  // If we sest a duration, fade it out
+  if (duration) {
+    setTimeout(function() {
+      osc.fade(0,0.2);
+    }, duration-50);
+  }
 }
 
 function draw() {
-  background(204, 120);
 
-  // write the text in black and get its bounding box
-  fill(0);
-  text(message, x, y);
-  bounds = font.textBounds(message,x,y,fontsize);
-
-  // check if the mouse is inside the bounding box and tickle if so
-  if ( mouseX >= bounds.x && mouseX <= bounds.x + bounds.w &&
-    mouseY >= bounds.y && mouseY <= bounds.y + bounds.h) {
-    x += random(-5, 5);
-    y += random(-5, 5);
+  // If we are autoplaying and it's time for the next note
+  if (autoplay && millis() > trigger){
+    playNote(notes[song[index].note], song[index].duration);
+    trigger = millis() + song[index].duration;
+    // Move to the next note
+    index ++;
+  // We're at the end, stop autoplaying.
+  } else if (index >= song.length) {
+    autoplay = false;
   }
+
+
+  // Draw a keyboard
+
+  // The width for each key
+  var w = width / notes.length;
+  for (var i = 0; i < notes.length; i++) {
+    var x = i * w;
+    // If the mouse is over the key
+    if (mouseX > x && mouseX < x + w && mouseY < height) {
+      // If we're clicking
+      if (mouseIsPressed) {
+        fill(244, 66, 101);
+      // Or just rolling over
+      } else {
+        fill(255, 204, 204);
+      }
+    } else {
+      fill(255);
+    }
+
+    // Or if we're playing the song, let's highlight it too
+    if (autoplay && i === song[index-1].note) {
+      fill(244, 66, 101);
+    }
+
+    // Draw the key
+    rect(x, 0, w-1, height-1);
+  }
+
+}
+
+// When we click
+function mousePressed() {
+  // Map mouse to the key index
+  var key = floor(map(mouseX, 0, width, 0, notes.length));
+  playNote(notes[key]);
+}
+
+// Fade it out when we release
+function mouseReleased() {
+  osc.fade(0,0.5);
 }
