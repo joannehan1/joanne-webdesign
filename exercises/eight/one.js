e1bd0f1c86fd5fca34c838a37cbf09217d0a0ac4
Joanne Han <jhan13@risd.edu>
2018-02-06 12:18:20 -0500

    yee

---
+++ exercises/eight/one.js
@@ -0,0 +1,4 @@
+// exercise 1
+for (var tag = '#'; tag.length < 8; tag = tag + '#') {
+	console.log(tag);
+}
---
+++ exercises/eight/two.js
@@ -0,0 +1,12 @@
+// exercise 2
+for (var n = 1; n <= 100; n++) {
+  if (n % 3 === 0 && n % 5 === 0) {
+  	console.log ('FizzBuzz')
+  } if (n % 3 === 0 ) {
+  	console.log ('Fizz')
+  } if (n % 5 === 0) {
+  	console.log ('Buzz')
+  } else {
+  	console.log(n);
+  }
+} 
